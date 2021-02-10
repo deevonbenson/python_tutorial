2345298b7fa36ba87124c6afabb77f080877563b
David Benson <dbenson3@MacBook-Pro.local>
2021-02-10 15:47:22 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Bilbo Baggins",
+    author_email="bbaggins@hobbitville.mu",
+    packages=["mysci"],
+    install_requires=[],
+)
